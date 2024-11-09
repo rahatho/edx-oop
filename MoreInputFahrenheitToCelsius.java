3d8955ad20a77bbf291dad59e1ebc2278ac4aec1
Rahat Ho <rahat.ho@gmail.com>
2024-11-09 16:44:46 +0600

    nextLine indent input

--- MoreInputFahrenheitToCelsius.java
+++ MoreInputFahrenheitToCelsius.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 
 public class MoreInputFahrenheitToCelsius {
-	public static void main (String[] args) {
+	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter a Fahrenheit value and the day of week: ");
 		int fahrenheit = input.nextInt();
-		
-		String day = input.next();
 
-		double celsius = (5/9.0) * (fahrenheit - 32);
-		
+		String day = input.nextLine();
+
+		double celsius = (5 / 9.0) * (fahrenheit - 32);
+
 		System.out.println(day + " Fahrenheit temparature " + fahrenheit);
 		System.out.println(day + " Celsius temparature " + celsius);
 	}
