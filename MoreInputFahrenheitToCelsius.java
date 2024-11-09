f8bde74d25f54b5381a44ec4a4fe73db67e74516
Rahat Ho <rahat.ho@gmail.com>
2024-11-09 16:09:47 +0600

    true single line input

--- MoreInputFahrenheitToCelsius.java
+++ MoreInputFahrenheitToCelsius.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class MoreInputFahrenheitToCelsius {
 	public static void main (String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.print("Enter the Fahrenheit Temparature: ");
+		System.out.print("Enter a Fahrenheit value and the day of week: ");
 		int fahrenheit = input.nextInt();
-		double celsius = (5/9.0) * (fahrenheit - 32);
 		
-		System.out.print("Enter day of week: ");
-		String day = input.nextLine();
+		String day = input.next();
+
+		double celsius = (5/9.0) * (fahrenheit - 32);
 		
 		System.out.println(day + " Fahrenheit temparature " + fahrenheit);
 		System.out.println(day + " Celsius temparature " + celsius);
